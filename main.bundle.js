392e2dff67e2411028967adefae91a56c81de34c
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2018-01-11 02:05:22 +0530

    x

--- main.bundle.js
+++ main.bundle.js
@@ -1745,7 +1745,7 @@ var ApiService = (function () {
     ApiService.prototype.getDatabaseProfile = function (env) {
         console.log('Getting tables for profile');
         console.log(env);
-        return this.httpC.get("http://10.224.69.47:9090/api/v1/listdatabase?source_type=hive&environment=" + env);
+        return this.httpC.get("/api/v1/listdatabase?source_type=hive&environment=" + env);
     };
     //curate
     ApiService.prototype.submitTagCorrections = function (params) {
