fcc32c527774d663864b979e1bddb297ba6d94ff
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2018-01-11 00:44:56 +0530

    x

--- main.bundle.js
+++ main.bundle.js
@@ -1733,6 +1733,7 @@ var ApiService = (function () {
     ApiService.prototype.submitTagCorrections = function (params) {
         console.log('IN CURATE API FE');
         console.log(params);
+        // {"zoneName": "lmb-datalake-hdp-store-raw-nonprod","sourceName": "avaya","tableName": "customer_log","columnName": "date_occurred_s","tagInfo": [{"tagType": "MIO","tagCategory": "MIO-PUBLIC"},{"tagType": "SEC","tagCategory": "SEC-SSN"},{"tagType": "DOM","tagCategory": "DOM-CUSTOMER"}]}
         try {
             return this.httpC.post('http://10.224.69.47:9090/v1/autotagging/tagcorrectionsubmit', params);
         }
