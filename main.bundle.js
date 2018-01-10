8d02a6baee576c7aa63d3bcd916f23831f2e7e10
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2018-01-11 01:15:05 +0530

    x

--- main.bundle.js
+++ main.bundle.js
@@ -1734,13 +1734,7 @@ var ApiService = (function () {
         console.log('IN CURATE API FE');
         console.log(params);
         // {"zoneName": "lmb-datalake-hdp-store-raw-nonprod","sourceName": "avaya","tableName": "customer_log","columnName": "date_occurred_s","tagInfo": [{"tagType": "MIO","tagCategory": "MIO-PUBLIC"},{"tagType": "SEC","tagCategory": "SEC-SSN"},{"tagType": "DOM","tagCategory": "DOM-CUSTOMER"}]}
-        try {
-            return this.httpC.post('http://10.224.69.47:9090/v1/autotagging/tagcorrectionsubmit', params);
-        }
-        catch (err) {
-            console.log(err);
-            throw err;
-        }
+        return this.httpC.post('http://10.224.69.47:9090/v1/autotagging/tagcorrectionsubmit', params);
     };
     //login stuff
     ApiService.prototype.login = function (params) {
